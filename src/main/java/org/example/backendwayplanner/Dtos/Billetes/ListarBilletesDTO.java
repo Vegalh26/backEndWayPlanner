fb3fd050282408701fa764d51a52439787a46727
Vegalh26 <velopezhernandez@safareyes.es>
2025-06-12 01:57:14 +0200

    Commits necesarios backend

--- src/main/java/org/example/backendwayplanner/Dtos/Billetes/ListarBilletesDTO.java
+++ src/main/java/org/example/backendwayplanner/Dtos/Billetes/ListarBilletesDTO.java
@@ -4,16 +4,22 @@ public class ListarBilletesDTO {
     // Atributos
     private Long id;
     private String nombre;
+    private String categoria;
+    private String pdf;
+    private Long viajeId;
 
     // Constructor
-    public ListarBilletesDTO(Long id, String nombre, String categoria, String ubicacion) {
+    public ListarBilletesDTO(Long id,String nombre, String categoria, String pdf, Long viajeId) {
         this.id = id;
         this.nombre = nombre;
+        this.categoria = categoria;
+        this.pdf = pdf;
+        this.viajeId = viajeId;
     }
 
     // Getters y Setters
     // ---------------------------------------
-    // Getter y Setter para id
+    // Getter y Setter para el ID
     public Long getId() {
         return id;
     }
@@ -22,12 +28,40 @@ public class ListarBilletesDTO {
         this.id = id;
     }
 
-    // Getter y Setter para nombre
+    // Getter y Setter para el Nombre
     public String getNombre() {
         return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    // Getter y Setter para la Categoria
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    // Getter y Setter para el PDF
+    public String getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(String pdf) {
+        this.pdf = pdf;
+    }
+
+
+    // Getter y Setter para el ID del Viaje
+    public Long getViajeId() {
+        return viajeId;
+    }
+
+    public void setViajeId(Long viajeId) {
+        this.viajeId = viajeId;
     }
 }
